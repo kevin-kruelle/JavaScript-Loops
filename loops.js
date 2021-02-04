be8f47ac0fd8ea17569048df08af625028e04c69
Kevin Kruelle <kkruelle2@gmail.com>
2021-02-04 15:48:28 -0500

    update JS do while loop

--- loops.js
+++ loops.js
@@ -1,15 +1,17 @@
 console.log('conected...')
 
-const generatedNumber = Math.floor(Math.random() * 20) + 1;
+const generatedNumber = Math.floor(Math.random() * 100) + 1;
 let numberOfGuesses = 0;
 
 do {
-    let userGuess = prompt('Guess a number between 1 and 20.')
+    let userGuess = prompt('Guess a number between 1 and 100.')
     numberOfGuesses++
     if (userGuess == generatedNumber) {
-        alert('You guessed the right number!')
+        document.querySelector('h1').innerHTML = 'You guessed the right number'
         break;
-    } else {
-        alert('wrong, try again')
+    } else if (userGuess < generatedNumber) {
+        alert('You guessed too low')
+    } else if (userGuess > generatedNumber) {
+        alert('You guessed too high.')
     }
 } while (numberOfGuesses < 5)
