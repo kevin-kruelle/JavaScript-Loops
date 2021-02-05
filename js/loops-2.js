2e8d92ae8287f5b127fbc0a340e02c4a9acbb4b8
Kevin Kruelle <kkruelle2@gmail.com>
2021-02-05 11:14:12 -0500

    right angle triangle pointing down

--- js/loops-2.js
+++ js/loops-2.js
@@ -5,23 +5,40 @@ console.log('conected...')
 2. Asign charactor that will create a triangle to a variable.
 3. Check to see if the number user typed in is a number.
 4.If user did not enter valid number, ask user to input a number again.
-4. Write a for loop that will
+4. Write a for loop
+5. Set a 
 */
 
 
 let userInput = prompt('Enter a number!');
 let character = '*';
-let triangle = ' ';
+
+//Right angle triangle pointing up
 
 if (isNaN(userInput)) {
     alert('You have not entered a number...refresh')
 } else {
 
 for (let i=1; i<= userInput; i++) {
-    triangle;
+    let space = ' ';
     for (let j=1; j<= i; j++) {
-        triangle += character;
+        space += character;
     } 
-     document.querySelector('p').innerHTML = (triangle + '<br>');
+    document.querySelector('p').innerHTML += space + '<br>';
+    }
+}
+
+//Right angle triangle pointing down
+
+if (isNaN(userInput)) {
+    alert('You have not entered a number...refresh')
+} else {
+
+for (let i=1; i<= userInput; i++) {
+    let space = ' ';
+    for (let j=userInput; j>= i; j--) {
+        space += character;
+    } 
+    document.querySelector('p').innerHTML += space + '<br>';
     }
 }
