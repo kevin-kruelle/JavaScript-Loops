ffcff09a2c9ccec3a39121a69b294f17e9eca473
Kevin Kruelle <kkruelle2@gmail.com>
2021-02-05 11:21:42 -0500

    added more psuedo code

--- js/loops-2.js
+++ js/loops-2.js
@@ -4,9 +4,15 @@ console.log('conected...')
 1. Ask the user to input a number.
 2. Asign charactor that will create a triangle to a variable.
 3. Check to see if the number user typed in is a number.
-4.If user did not enter valid number, ask user to input a number again.
-4. Write a for loop
-5. Set a 
+4. If user did not enter valid number, ask user to input a number again.
+5. If user did enter a number, create and else statement with a for loop nested.
+6. define the first for loop. i equal to 1.
+7. loop i will run until it is less than or equal to userInput.
+8. if i is less than or equal to userInput, add 1.
+9. define a variable that will hold a space in between each character so that the loop resets each time around.
+10. Create a nested loop inside of the first for loop.
+11. j = 1
+12. let j run and increment by 1 until it is greater than the first loop.
 */
 
 
